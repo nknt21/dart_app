4de7fa0375d8ff68673ca812c1465f19b6a3803a
nknt21 <nknt21@outlook.com>
2025-03-31 12:32:15 +0900

    Update main.dart

--- my_dart_app/my_dart_app/my_dart_app/lib/main.dart
+++ my_dart_app/my_dart_app/my_dart_app/lib/main.dart
@@ -1,3 +1,17 @@
-int calculate() {
-  return 6 * 7;
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '分割したコードの例',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomeScreen(),  // ホーム画面を起動
+    );
+  }
 }
